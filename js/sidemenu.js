dc1734cb08df4b3db1ff1063c5e1109eabc3b81b
Aiko Takemura <aiko.takemura@gmail.com>
2019-10-08 17:19:08 -0400

    Edit header and navbar

---
+++ js/sidemenu.js
@@ -0,0 +1,12 @@
+let openSideMenu = document.querySelector('.fa-bars');
+openSideMenu.addEventListener('click', function() {
+    document.getElementById('side-menu').style.width = '100%';
+    // When clicking the hamburger icon for the first time, the side menu appears abruptly. From the second time onwards it shows up from the side. Is there a way to make the menu show up from the side from the first click?
+    document.getElementById('side-menu').style.display = 'block';
+});
+
+// I want to click on the hamburger icon to close the sidemenu instead of the x button.. Do you know an easy way to do this?
+let closeSideMenu = document.querySelector('.btn-close');
+closeSideMenu.addEventListener('click', function() {
+    document.getElementById('side-menu').style.width = '0';
+});
